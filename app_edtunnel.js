b7febad361d3b1ff271761dd0ab00f67fba267aa
csl83 <127131407+csl83@users.noreply.github.com>
2023-07-27 22:27:54 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '938911f6-c3e2-41ba-b6a8-590ca3bdae80';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
